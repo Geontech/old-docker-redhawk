af4f66e308c89077ecbffb6adcf97002fbeacab2
Patrick Wolfram <pwolfram@geontech.com>
2016-05-12 17:18:40 -0400

    Fixed all errors in copyright header for runAsDomain.sh file.

--- runAsDomain.sh
+++ runAsDomain.sh
@@ -5,12 +5,12 @@
 #
 # This file is part of Docker REDHAWK.
 #
-# REDHAWK rest-python is free software: you can redistribute it and/or modify it under
+# Docker REDHAWK is free software: you can redistribute it and/or modify it under
 # the terms of the GNU Lesser General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option) any
 # later version.
 #
-# REDHAWK rest-python is distributed in the hope that it will be useful, but WITHOUT
+# Docker REDHAWK is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
 # details.
